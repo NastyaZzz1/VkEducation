bdf447a757e3724cefe66edb1aac38335ce2654a
Nastya Zakharova <zaharova_nastya_2004@mail.ru>
2026-03-20 18:04:20 +0500

    Импортировала coil3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.coil.compose.network)
+    implementation(libs.coil.compose)
     implementation(libs.androidx.navigation)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
