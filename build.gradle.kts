51a13e90d13ec36f1b2472782bf35a6588cd6b37
Nastya Zakharova <zaharova_nastya_2004@mail.ru>
2026-03-26 21:25:56 +0500

    Добавление зависимостей

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.kotlin.plugin.parcelize) apply false
     alias(libs.plugins.google.dagger.hilt.android) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
