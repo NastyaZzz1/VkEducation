9b7f0253317b3f94ae46ab394cd8a8b63da542fd
Nastya Zakharova <zaharova_nastya_2004@mail.ru>
2026-04-08 00:15:50 +0500

    Небольшие правки

--- app/src/main/java/com/nastya/vkeducation/domain/GetAppDetailsUseCase.kt
+++ app/src/main/java/com/nastya/vkeducation/domain/GetAppDetailsUseCase.kt
@@ -1,6 +1,7 @@
 package com.nastya.vkeducation.domain
 
 import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
 
 class GetAppDetailsUseCase @Inject constructor(
     private val appRepository: AppRepository
